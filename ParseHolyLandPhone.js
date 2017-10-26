dcf6b6d9990a76532d3ab12da9dc428bf54f1fbf
Mordi Sacks <mordi.sacks@triple.co.il>
2017-10-26 17:01:32 +0300

    Initial commit...

---
+++ ParseHolyLandPhone.js
@@ -0,0 +1,168 @@
+/**
+ * @author Mordi Sacks
+ * @class ParseHolyLandPhone
+ */
+export class ParseHolyLandPhone {
+
+    constructor(phoneNumber) {
+        this.phoneNumber = phoneNumber;
+        this.fromInternational();
+    }
+
+    static create(phoneNumber) {
+        return new ParseHolyLandPhone(phoneNumber);
+    }
+
+
+    /**
+     * Checks if phone number is a valid Israeli/Palestinian phone number
+     *
+     * @return boolean
+     */
+    isValid() {
+        return this.phoneNumber.match(/^((0[23489][2356789]|0[57][102345689]\d|1(2(00|12)|599|70[05]|80[019]|90[012]|919))\d{6}|\*\d{4})$/) !== null;
+    }
+
+    /**
+     * Checks if phone number is Israeli
+     *
+     * @return boolean
+     */
+    isIsraeli() {
+        return this.phoneNumber.match(/^((0[23489][356789]|0[57][1023458]\d|1(2(00|12)|599|70[05]|80[019]|90[012]|919))\d{6}|\*\d{4})$/) !== null;
+    }
+
+    /**
+     * Checks if phone number is Palestinian
+     *
+     * @return boolean
+     */
+    isPalestinian() {
+        return this.phoneNumber.match(/^(0[23489]2|05[69]\d|)\d{6}$/) !== null;
+    }
+
+    /**
+     * Checks if phone number is land line
+     *
+     * @return boolean
+     */
+    isLandLine() {
+        return this.phoneNumber.match(/^0([23489][2356789]|7\d{2})\d{6}$/) !== null;
+    }
+
+    /**
+     * Checks if phone number is mobile
+     *
+     * @return boolean
+     */
+    isMobile() {
+        return this.phoneNumber.match(/^0[5][102345689]\d{7}$/) !== null;
+    }
+
+    /**
+     * Checks if phone number is special (*1234)
+     *
+     * @return boolean
+     */
+    isSpecial() {
+        return this.phoneNumber.match(/^\*\d{4}$/) !== null;
+    }
+
+    /**
+     * Checks if phone number is business (1800, etc)
+     *
+     * @return boolean
+     */
+    isBusiness() {
+        return this.phoneNumber.match(/^1(2(00|12)|599|70[05]|80[019]|90[012]|919)\d{6}$/) !== null;
+    }
+
+    /**
+     * Checks if phone number is toll free (1800)
+     *
+     * @return boolean
+     */
+    isTollFree() {
+        return this.phoneNumber.match(/^180[019]\d{6}$/) !== null;
+    }
+
+    /**
+     * Checks if phone number is premium (1900)
+     *
+     * @return boolean
+     */
+    isPremium() {
+        return this.phoneNumber.match(/^19(0[012]|19)\d{6}$/) !== null;
+    }
+
+    /**
+     * Checks if phone number is Kosher (phone supports only calls)
+     *
+     * @return boolean
+     */
+    isKosher() {
+        return this.phoneNumber.match(/^0([23489]80|5041|5271|5276|5484|5485|5331|5341|5832|5567)\d{5}$/) !== null;
+    }
+
+    /**
+     * Checks if phone number is erotic (1919)
+     *
+     * @return boolean
+     */
+    isErotic() {
+        return this.phoneNumber.match(/^1919\d{6}$/) !== null;
+    }
+
+    fromInternational() {
+        this.phoneNumber = this.phoneNumber.replace(/^(972)(\d{8,9})$/, '0$2');
+    }
+
+    /**
+     * Returns phone number transformed to international format
+     * 021231234  > 97221231234
+     * 0501231234 > 972501231234
+     */
+    getInternational() {
+        return this.phoneNumber.replace(/^(0)(\d{8,9})$/, '972$2');
+    }
+
+    isNotValid(){
+        return !this.isValid();
+    }
+
+    isNotIsraeli(){
+        return !this.isIsraeli();
+    }
+
+    isNotPalestinian(){
+        return !this.isPalestinian();
+    }
+
+    isNotLandLine(){
+        return !this.isLandLine();
+    }
+
+    isNotMobile(){
+        return !this.isMobile();
+    }
+
+    isNotBusiness(){
+        return !this.isBusiness();
+    }
+
+    isNotTollFree(){
+        return !this.isTollFree();
+    }
+
+    isNotPremium(){
+        return !this.isPremium();
+    }
+
+    isNotKosher(){
+        return !this.isKosher();
+    }
+
+    isNotErotic(){
+        return !this.isErotic();
+    }
+}
---
+++ readme.md
@@ -0,0 +1,30 @@
+# ParseHolyLandPhone
+Parse Holy land (Israel/Palestine) phone numbers
+
+
+## usage
+```ecmascript 6
+// via new instance
+var number = new ParseHolyLandPhone('025121234');
+
+// via static method
+var number = ParseHolyLandPhone.create('025121234');
+
+number.isValid();
+```
+
+## Available methods
+* `isValid` Checks if phone number is a valid Israeli/Palestinian phone number.
+* `isIsraeli` Checks if phone number is Israeli.
+* `isPalestinian` Checks if phone number is Palestinian.
+* `isLandLine` Checks if phone number is LandLine.
+* `isSpecial` Checks if phone number is Special (*1234).
+* `isMobile` Checks if phone number is Mobile.
+* `isBusiness` Checks if phone number is Business (1700, 1800, etc).
+* `isTollFree` Checks if phone number is TollFree (1800, etc).
+* `isPremium` Checks if phone number is Premium (1900, etc).
+* `isKosher` Checks if phone number is Kosher (phone supports only calls).
+* `isErotic` Checks if phone number is Erotic (1919).
+* `getInternational` Returns phone number transformed to international format 025231234  > 97225231234.
+* `getPhoneNumber` Returns the parsed phone number.
+### all `is` functions have a reverse function `isNot`.
